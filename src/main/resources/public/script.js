a297b97f39d71ff2cb4a9c755158b046bd1fac7c
hebarachid <67964217+hebarachid@users.noreply.github.com>
2022-04-30 01:42:11 +0300

    redering results on frontend

--- src/main/resources/public/script.js
+++ src/main/resources/public/script.js
@@ -1,6 +1,5 @@
 function sendData(){
 let n=document.getElementById('txt-input1').value
-console.log(n);
 let op_rate=document.getElementById('txt-input2').value;
 let bsize=document.getElementById('txt-input3').value;
 let spkt=document.getElementById('txt-input4').value;
@@ -17,3 +16,57 @@ fetch('http://localhost:8080/api/leakyparameters', {
 }).then(res => res.json())
   .then(res => console.log(res));
 };
+
+function fetchandstart(){
+ var xmlHttp = new XMLHttpRequest();
+   xmlHttp.open( "GET", 'http://localhost:8080/api/getresult', false ); // false for synchronous request
+   xmlHttp.send( null );
+   const leaky = JSON.parse(xmlHttp.responseText);
+   drawtable(leaky);
+}
+function drawtable(leaky){
+        var col = [];
+        for (var i = 0; i < leaky.length; i++) {
+            for (var key in leaky[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                }
+            }
+        }
+
+        // CREATE DYNAMIC TABLE.
+        var table = document.createElement("table");
+        var tr = table.insertRow(-1);                   // TABLE ROW.
+        for (var i = 0; i < col.length; i++) {
+            var th = document.createElement("th");      // TABLE HEADER.
+            th.innerHTML = col[i];
+            tr.appendChild(th);
+        }
+
+        // ADD JSON DATA TO THE TABLE AS ROWS.
+        for (var i = 0; i < leaky.length; i++) {
+            tr = table.insertRow(-1);
+            for (var j = 0; j < col.length; j++) {
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML =leaky[i][col[j]];
+            }
+        }
+        var divContainer = document.getElementById("showData");
+        divContainer.innerHTML = "";
+        divContainer.appendChild(table);
+    }
+let wave = document.querySelector(".wave");
+console.log(wave)
+var bottom=-400;
+
+window.addEventListener('load',function () {
+  move();
+});
+function move(){
+var id=setInterval(frame,2000);
+function frame(){
+  wave.style.bottom=120;
+  bottom=bottom-25;
+  console.log(bottom);
+}
+}
