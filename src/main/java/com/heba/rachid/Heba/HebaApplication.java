5936b59bdb30fe2c45e7d7e12ec93af4070f1851
hebarachid <67964217+hebarachid@users.noreply.github.com>
2022-04-29 12:14:04 +0300

    Initial commit

---
+++ src/main/java/com/heba/rachid/Heba/HebaApplication.java
@@ -0,0 +1,13 @@
+package com.heba.rachid.Heba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heba/rachid/Heba/HebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heba.rachid.Heba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
